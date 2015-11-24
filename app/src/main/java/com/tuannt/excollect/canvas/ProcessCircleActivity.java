bd583834e834bf6e3315142ab58c54774d420936
Nguyen Thanh Tuan <tuannt@asiantech.vn>
2015-11-24 11:31:30 +0700

    ex draw circle process

---
+++ app/src/main/java/com/tuannt/excollect/canvas/ProcessCircleActivity.java
@@ -0,0 +1,19 @@
+package com.tuannt.excollect.canvas;
+
+import android.os.Bundle;
+
+import com.tuannt.excollect.BaseActivity;
+import com.tuannt.excollect.R;
+
+/**
+ * Comment
+ *
+ * @author TuanNT
+ */
+public class ProcessCircleActivity extends BaseActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_process_circle);
+    }
+}
