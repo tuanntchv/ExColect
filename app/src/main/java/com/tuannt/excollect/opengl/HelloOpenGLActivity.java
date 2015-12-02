9f339feee7c015d57370f983000ec626e673d13e
tuanntchv <tuannt.chv@gmail.com>
2015-12-02 22:22:51 +0700

    draw table

--- app/src/main/java/com/tuannt/excollect/opengl/HelloOpenGLActivity.java
+++ app/src/main/java/com/tuannt/excollect/opengl/HelloOpenGLActivity.java
@@ -19,7 +19,7 @@ public class HelloOpenGLActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         mGlSurfaceView = new GLSurfaceView(this);
         mGlSurfaceView.setEGLContextClientVersion(2);
-        mGlSurfaceView.setRenderer(new HelloOpenGLRender());
+        mGlSurfaceView.setRenderer(new HelloOpenGLRender(this));
         setContentView(mGlSurfaceView);
     }
 
--- app/src/main/java/com/tuannt/excollect/opengl/HelloOpenGLRender.java
+++ app/src/main/java/com/tuannt/excollect/opengl/HelloOpenGLRender.java
@@ -5,33 +5,130 @@ import android.opengl.GLSurfaceView;
 
 import com.tuannt.excollect.R;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
 import static android.opengl.GLES20.glClear;
 import static android.opengl.GLES20.glClearColor;
+import static android.opengl.GLES20.glDrawArrays;
+import static android.opengl.GLES20.glEnableVertexAttribArray;
+import static android.opengl.GLES20.glGetAttribLocation;
+import static android.opengl.GLES20.glGetUniformLocation;
+import static android.opengl.GLES20.glUniform4f;
+import static android.opengl.GLES20.glUseProgram;
+import static android.opengl.GLES20.glVertexAttribPointer;
 import static android.opengl.GLES20.glViewport;
-
+import static android.opengl.GLES20.GL_FLOAT;import static android.opengl.GLES20.GL_TRIANGLES;import static android.opengl.GLES20.GL_LINES;
+import static android.opengl.GLES20.GL_POINTS;
 /**
  * Comment
  *
  * @author TuanNT
  */
 public class HelloOpenGLRender implements GLSurfaceView.Renderer {
+    private static final String U_COLOR = "u_Color";
+    private static final String A_POSITION = "a_Position";
+    private static final int BYTES_PER_FLOAT = 4;
+    private static final int POSITION_COMPONENT_COUNT = 2;
+
+    private int uColorLocation;
+    private int aPositionLocation;
+
+    private final FloatBuffer vertexData;
+
     private Context mContext;
+    private int mProgram;
 
     public HelloOpenGLRender(Context context) {
         this.mContext = context;
+
+              /*
+		float[] tableVertices = {
+			0f,  0f,
+			0f, 14f,
+			9f, 14f,
+			9f,  0f
+		};
+         */
+//        float[] tableVerticesWithTriangles = {
+//                // Triangle 1
+//                0f,  0f,
+//                9f, 14f,
+//                0f, 14f,
+//
+//                // Triangle 2
+//                0f,  0f,
+//                9f,  0f,
+//                9f, 14f,
+//                // Next block for formatting purposes
+////                9f, 14f,
+//                 // Comma here for formatting purposes
+//
+//                // Line 1
+//                0f,  7f,
+//                9f,  7f,
+//
+//                // Mallets
+//                4.5f,  2f,
+//                4.5f, 12f
+//        };
+        float[] tableVerticesWithTriangles = {
+                // Triangle 1
+                -0.5f, -0.5f,
+                0.5f,  0.5f,
+                -0.5f,  0.5f,
+
+                // Triangle 2
+                -0.5f, -0.5f,
+                0.5f, -0.5f,
+                0.5f,  0.5f,
+
+                // Line 1
+                -0.5f, 0f,
+                0.5f, 0f,
+
+                // Mallets
+                0f, -0.25f,
+                0f,  0.25f
+        };
+
+        vertexData = ByteBuffer
+                .allocateDirect(tableVerticesWithTriangles.length * BYTES_PER_FLOAT)
+                .order(ByteOrder.nativeOrder())
+                .asFloatBuffer();
+
+        vertexData.put(tableVerticesWithTriangles);
     }
 
     @Override
     public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
-        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+//        glClearColor(1.0f, 0.0f, 0.0f, 0.0f); red color
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
         String vertexShaderSource = TextResourceReader.readTextFileFromResource(mContext, R.raw.simple_vertex_shader);
         String fragmentShaderSource = TextResourceReader.readTextFileFromResource(mContext, R.raw.simple_fragment_shader);
         int vertexShader = ShaderHelper.compileVertexShader(vertexShaderSource);
         int fragmentShader = ShaderHelper.compileFragmentShader(fragmentShaderSource);
+        mProgram = ShaderHelper.linkProgram(vertexShader, fragmentShader);
+
+        if (LoggerConfig.ON) {
+            ShaderHelper.validateProgram(mProgram);
+        }
+        glUseProgram(mProgram);
+
+        uColorLocation = glGetUniformLocation(mProgram, U_COLOR);
+        aPositionLocation = glGetAttribLocation(mProgram, A_POSITION);
+
+        vertexData.position(0);
+        glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GL_FLOAT,
+                false, 0, vertexData);
+
+        glEnableVertexAttribArray(aPositionLocation);
+
     }
 
     @Override
@@ -42,5 +139,24 @@ public class HelloOpenGLRender implements GLSurfaceView.Renderer {
     @Override
     public void onDrawFrame(GL10 gl10) {
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUniform4f(uColorLocation, 1.0f, 1.0f, 1.0f, 1.0f);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+
+        // Draw the table.
+        glUniform4f(uColorLocation, 1.0f, 1.0f, 1.0f, 1.0f);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+
+        // Draw the center dividing line.
+        glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
+        glDrawArrays(GL_LINES, 6, 2);
+
+        // Draw the first mallet blue.
+        glUniform4f(uColorLocation, 0.0f, 0.0f, 1.0f, 1.0f);
+        glDrawArrays(GL_POINTS, 8, 1);
+
+        // Draw the second mallet red.
+        glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
+        glDrawArrays(GL_POINTS, 9, 1);
     }
 }
