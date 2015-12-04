9a8b49e898981fe5024e62ec2999a1ea5aaefae4
Nguyen Thanh Tuan <tuannt@asiantech.vn>
2015-12-04 08:22:27 +0700

    backup

--- app/src/main/java/com/tuannt/excollect/opengl/HelloOpenGLRender.java
+++ app/src/main/java/com/tuannt/excollect/opengl/HelloOpenGLRender.java
@@ -13,33 +13,43 @@ import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
+import static android.opengl.GLES20.GL_FLOAT;
+import static android.opengl.GLES20.GL_POINTS;
+import static android.opengl.GLES20.GL_LINES;
+import static android.opengl.GLES20.GL_TRIANGLE_FAN;
 import static android.opengl.GLES20.glClear;
 import static android.opengl.GLES20.glClearColor;
 import static android.opengl.GLES20.glDrawArrays;
 import static android.opengl.GLES20.glEnableVertexAttribArray;
 import static android.opengl.GLES20.glGetAttribLocation;
 import static android.opengl.GLES20.glGetUniformLocation;
-import static android.opengl.GLES20.glUniform4f;
+import static android.opengl.GLES20.glUniformMatrix4fv;
 import static android.opengl.GLES20.glUseProgram;
 import static android.opengl.GLES20.glVertexAttribPointer;
 import static android.opengl.GLES20.glViewport;
-import static android.opengl.GLES20.GL_FLOAT;import static android.opengl.GLES20.GL_TRIANGLES;import static android.opengl.GLES20.GL_LINES;
-import static android.opengl.GLES20.GL_POINTS;
+import static android.opengl.Matrix.orthoM;
+
 /**
  * Comment
  *
  * @author TuanNT
  */
 public class HelloOpenGLRender implements GLSurfaceView.Renderer {
-    private static final String U_COLOR = "u_Color";
     private static final String A_POSITION = "a_Position";
-    private static final int BYTES_PER_FLOAT = 4;
+    private static final String A_COLOR = "a_Color";
+    private static final String U_MATRIX = "u_Matrix";
     private static final int POSITION_COMPONENT_COUNT = 2;
+    private static final int COLOR_COMPONENT_COUNT = 3;
+    private static final int BYTES_PER_FLOAT = 4;
+    private static final int STRIDE = (POSITION_COMPONENT_COUNT + COLOR_COMPONENT_COUNT) * BYTES_PER_FLOAT;
 
-    private int uColorLocation;
+    //    private int uColorLocation;
     private int aPositionLocation;
+    private int aColorLocation;
+    private int uMatrixLocation;
 
     private final FloatBuffer vertexData;
+    private final float[] projectionMatrix = new float[16];
 
     private Context mContext;
     private int mProgram;
@@ -48,45 +58,45 @@ public class HelloOpenGLRender implements GLSurfaceView.Renderer {
         this.mContext = context;
 
               /*
-		float[] tableVertices = {
+        float[] tableVertices = {
 			0f,  0f,
 			0f, 14f,
 			9f, 14f,
 			9f,  0f
 		};
          */
-//        float[] tableVerticesWithTriangles = {
-//                // Triangle 1
-//                0f,  0f,
+      /*  float[] tableVerticesWithTriangles = {
+                // Triangle 1
+                0f,  0f,
+                9f, 14f,
+                0f, 14f,
+
+                // Triangle 2
+                0f,  0f,
+                9f,  0f,
+                9f, 14f,
+                // Next block for formatting purposes
 //                9f, 14f,
-//                0f, 14f,
-//
-//                // Triangle 2
-//                0f,  0f,
-//                9f,  0f,
-//                9f, 14f,
-//                // Next block for formatting purposes
-////                9f, 14f,
-//                 // Comma here for formatting purposes
-//
-//                // Line 1
-//                0f,  7f,
-//                9f,  7f,
-//
-//                // Mallets
-//                4.5f,  2f,
-//                4.5f, 12f
-//        };
-        float[] tableVerticesWithTriangles = {
+                 // Comma here for formatting purposes
+
+                // Line 1
+                0f,  7f,
+                9f,  7f,
+
+                // Mallets
+                4.5f,  2f,
+                4.5f, 12f
+        };*/
+/*        float[] tableVerticesWithTriangles = {
                 // Triangle 1
                 -0.5f, -0.5f,
-                0.5f,  0.5f,
-                -0.5f,  0.5f,
+                0.5f, 0.5f,
+                -0.5f, 0.5f,
 
                 // Triangle 2
                 -0.5f, -0.5f,
                 0.5f, -0.5f,
-                0.5f,  0.5f,
+                0.5f, 0.5f,
 
                 // Line 1
                 -0.5f, 0f,
@@ -94,7 +104,42 @@ public class HelloOpenGLRender implements GLSurfaceView.Renderer {
 
                 // Mallets
                 0f, -0.25f,
-                0f,  0.25f
+                0f, 0.25f,
+        };*/
+
+     /*   float[] tableVerticesWithTriangles = {
+                // Triangle Fan
+                0, 0,
+                -0.5f, -0.5f,
+                0.5f, -0.5f,
+                0.5f, 0.5f,
+                -0.5f, 0.5f,
+                -0.5f, -0.5f,
+
+                // Line 1
+                -0.5f, 0f,
+                0.5f, 0f,
+
+                // Mallets
+                0f, -0.25f,
+                0f, 0.25f
+        };*/
+
+
+        float[] tableVerticesWithTriangles = {
+                // Triangle Fan
+                0f,    0f,   1f,   1f,   1f,
+                -0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
+                0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
+                0.5f,  0.8f, 0.7f, 0.7f, 0.7f,
+                -0.5f,  0.8f, 0.7f, 0.7f, 0.7f,
+                -0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
+// Line 1
+                -0.5f, 0f, 1f, 0f, 0f,
+                0.5f, 0f, 1f, 0f, 0f,
+// Mallets
+                0f, -0.4f, 0f, 0f, 1f,
+                0f,  0.4f, 1f, 0f, 0f
         };
 
         vertexData = ByteBuffer
@@ -120,43 +165,59 @@ public class HelloOpenGLRender implements GLSurfaceView.Renderer {
         }
         glUseProgram(mProgram);
 
-        uColorLocation = glGetUniformLocation(mProgram, U_COLOR);
+        //  uColorLocation = glGetUniformLocation(mProgram, U_COLOR);
         aPositionLocation = glGetAttribLocation(mProgram, A_POSITION);
+        aColorLocation = glGetAttribLocation(mProgram, A_COLOR);
+        uMatrixLocation = glGetUniformLocation(mProgram, U_MATRIX);
 
         vertexData.position(0);
-        glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GL_FLOAT,
-                false, 0, vertexData);
-
+        glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GL_FLOAT, false, STRIDE, vertexData);
         glEnableVertexAttribArray(aPositionLocation);
+
+        vertexData.position(POSITION_COMPONENT_COUNT);
+        glVertexAttribPointer(aColorLocation, COLOR_COMPONENT_COUNT, GL_FLOAT, false, STRIDE, vertexData);
+        glEnableVertexAttribArray(aColorLocation);
+
+
 
     }
 
     @Override
     public void onSurfaceChanged(GL10 gl10, int width, int height) {
         glViewport(0, 0, width, height);
+        final float aspectRatio = width > height ?
+                (float) width / (float) height :
+                (float) height / (float) width;
+        if (width > height) {
+            // Landscape
+            orthoM(projectionMatrix, 0, -aspectRatio, aspectRatio, -1f, 1f, -1f, 1f);
+        } else {
+            // Portrait or square
+            orthoM(projectionMatrix, 0, -1f, 1f, -aspectRatio, aspectRatio, -1f, 1f);
+        }
     }
 
     @Override
     public void onDrawFrame(GL10 gl10) {
         glClear(GL_COLOR_BUFFER_BIT);
-
-        glUniform4f(uColorLocation, 1.0f, 1.0f, 1.0f, 1.0f);
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glUniformMatrix4fv(uMatrixLocation, 1, false, projectionMatrix, 0);
 
         // Draw the table.
-        glUniform4f(uColorLocation, 1.0f, 1.0f, 1.0f, 1.0f);
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+        //   glUniform4f(uColorLocation, 1.0f, 1.0f, 1.0f, 1.0f);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 6);
 
         // Draw the center dividing line.
-        glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
+        //    glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
         glDrawArrays(GL_LINES, 6, 2);
 
         // Draw the first mallet blue.
-        glUniform4f(uColorLocation, 0.0f, 0.0f, 1.0f, 1.0f);
+        //   glUniform4f(uColorLocation, 0.0f, 0.0f, 1.0f, 1.0f);
         glDrawArrays(GL_POINTS, 8, 1);
 
         // Draw the second mallet red.
-        glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
+        //     glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
         glDrawArrays(GL_POINTS, 9, 1);
+        // Draw the table.
+
     }
 }
